7fce4c286a3b2c4971974ff3e99215636f5eb290
praveen dodla <dodlapraveen09@gmail.com>
2022-07-26 16:07:38 +0530

    initial uc1

---
+++ empuc1/empuc1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace empuc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            if(name == "Praveen")
+            {
+                Console.WriteLine("Name is ok");
+            }
+            else
+            {
+                Console.WriteLine("Name is not ok");
+            }
+        }
+    }
+}
